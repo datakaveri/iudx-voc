5e34a7e6c24b2420c20bc46e06e0852f4aa0d97c
MonikaSonali <monikasonali96@gmail.com>
2022-09-27 16:08:21 +0530

    gen_properties script updated, new properties added to WmgmtVehicle

--- utils/misc/data-model-properties/gen_properties.py
+++ utils/misc/data-model-properties/gen_properties.py
@@ -1,4 +1,6 @@
-#! /usr/local/bin/python3
+
+import deepdiff
+# deepdiff 5.8.1
 
 import csv
 import json
@@ -9,20 +11,29 @@ import pandas as pd
 import os, fnmatch
 from pathlib import Path
 import os.path as path
-
+from tkinter import Tk
+from tkinter import filedialog
+# tk 8.6
 """ Add the property names to ignore list to skip property generation. """
 #ignore = ["Custom", "location", "deviceModel", "rainfallMeasured", "rainfallForecast", "name", ""]
 
-dir_home = path.abspath(path.join(os.path.abspath(os.getcwd()) ,"../../.."))
-file_dir = (os.path.abspath(os.getcwd()) + "/" + input("Enter file name without extension\n")+".xlsx")
 
+Tk().withdraw() 
+file_dir = filedialog.askopenfilename(title="Select A File")
+dir_home = path.abspath(path.join(os.path.abspath(file_dir) ,"../../../.."))
+
+with open(dir_home+"/utils/misc/data-model-properties/template.jsonld", "r") as template:
+    obj = json.load(template)
 
 def check_dir(file_path):
     if not os.path.exists(file_path):
         os.makedirs(file_path)
 
-with open(dir_home+"/utils/misc/data-model-properties/template.jsonld", "r") as template:
-    obj = json.load(template)
+def write_json(base_prop,base_file):
+    ordered_prop = order_obj(base_prop)
+    base_file.seek(0)
+    json.dump(ordered_prop, base_file, indent=4)
+    base_file.truncate()
 
 def check_multiple_includes(includes):
     if "," in includes:
@@ -31,7 +42,6 @@ def check_multiple_includes(includes):
         includes_list = []
         includes_list.append(includes)
     return includes_list
-
 def which_iudx_property(prop_type):
     if prop_type == "QP":
         prop_list = []
@@ -184,6 +194,8 @@ def gen_properties(df):
             csv_match = item[5].strip()
             csv_domain_list = check_multiple_includes(csv_domain)
             csv_range_list = check_multiple_includes(csv_range)
+            properties_path = model_name_dir + "/properties/" + csv_label + ".jsonld"
+
             if "@graph" in obj.keys():
                 new_list = []
                 tmp_obj = OrderedDict()
@@ -195,33 +207,45 @@ def gen_properties(df):
                 tmp_obj["rdfs:isDefinedBy"] = obj["@graph"][0]["rdfs:isDefinedBy"]
                 tmp_obj["iudx:domainIncludes"] = add_domain_or_range(csv_domain_list, csv_domain)
                 tmp_obj["iudx:rangeIncludes"] = add_domain_or_range(csv_range_list, csv_range)
-                if csv_match:
-                    tmp_obj["skos:exactMatch"] = add_similar_match(csv_match)
+                if csv_match != 'nan':
+                    tmp_obj["skos:exactMatch"] = {'@id': csv_match}
                 new_dict["@graph"] = new_list
-                
-                properties_path = model_name_dir + "/properties/" + csv_label + ".jsonld"
-                check_dir( model_name_dir + "/properties" )
 
-                if not os.path.exists(properties_path):
-                    with open(properties_path, "w+") as prop_file:
-                        json.dump(new_dict, prop_file, indent=4)
-                        print("New Property - ", csv_label,".jsonld added")
+                check_dir(model_name_dir + "/properties" )
 
-                else:
-                    with open(properties_path,"r+") as base_file:
+                if os.path.exists(properties_path):
+                    with open(properties_path, "r+") as base_file:
                         base_prop = json.load(base_file)
-                        csv_domain = csv_domain.split(",")
-                        for i in range(len(csv_domain)):
-                            new_domain = csv_domain[i].strip()
-                            new_domain_obj = {"@id": "iudx:"+new_domain}
-                            if new_domain_obj not in base_prop["@graph"][0]["iudx:domainIncludes"]:
-                                base_prop["@graph"][0]["iudx:domainIncludes"].append(new_domain_obj)
-                                ordered_prop = order_obj(base_prop)
-                                base_file.seek(0)
-                                json.dump(ordered_prop, base_file, indent=4)
-                                base_file.truncate()
+                        diff = deepdiff.DeepDiff(base_prop, json.loads(json.dumps(new_dict)))
+                        if diff:
+                            if list(diff.keys()).count("values_changed"):
+                                print("Existing Property: ", csv_label, " - Value Changes -> ",
+                                      [k.split("root['@graph'][0]")[1] for k, v in diff["values_changed"].items()])
+                            if list(diff.keys()).count("iterable_item_added"):
+                                print("Existing Property: ", csv_label, " - Value Additions -> ",
+                                      [k.split("root['@graph'][0]")[1] for k, v in diff["iterable_item_added"].items()])
+                            if csv_match != 'nan':
+                                base_prop["@graph"][0]["skos:exactMatch"] = {'@id': csv_match}
+                            
+                            base_prop["@graph"][0]["@type"] = which_iudx_property(csv_type)
+                            base_prop["@graph"][0]["rdfs:comment"] = csv_comment
+                            base_prop["@graph"][0]["iudx:rangeIncludes"] = add_domain_or_range(csv_range_list, csv_range)
+
+                            csv_domain = csv_domain.split(",")
+                            for i in range(len(csv_domain)):
+                                new_domain = csv_domain[i].strip()
+                                new_domain_obj = {"@id": "iudx:"+new_domain}
+                                if new_domain_obj not in base_prop["@graph"][0]["iudx:domainIncludes"]:
+                                    base_prop["@graph"][0]["iudx:domainIncludes"].append(new_domain_obj)
+                            write_json(base_prop,base_file)
+                
+                if not os.path.exists(properties_path):
+                     with open(properties_path, "w+") as prop_file:
+                        json.dump(new_dict, prop_file, indent=4)
+                        print("New Property added - ", csv_label+".jsonld")
+
             else:
-                print("@graph missing in  ", csv_label)
+                print("@graph missing in  "+ csv_label)
         if item[8] == "1":
             base_property = item[0]
             new_domain = item[3]
@@ -233,7 +257,7 @@ def gen_properties(df):
                     ordered_prop = order_obj(base_prop)
                     base_file.seek(0)
                     json.dump(ordered_prop, base_file, indent=4)
-                    print("Base Schema - ", base_property, ".jsonld updated")
+                    print("Base Schema Property: ", base_property+" updated with new domain ->", new_domain_obj)
                     base_file.truncate()
         
         if item[7] == "1":
@@ -241,10 +265,8 @@ def gen_properties(df):
             try:
                 base_property = item[0]
                 domains = item[3]
-                # base_property = "route_id"
-                # dir_home = "/home/monika/Documents/IUDX-VOC/iudx-voc"
+
                 property_path = find_name(base_property+'.jsonld', dir_home)
-                # print("base_property", base_property, "property_path[0] ", property_path[0])
                 with open(property_path[0],"r+") as base_file:
                     base_prop = json.load(base_file)
                     domains = domains.split(",")
@@ -252,30 +274,30 @@ def gen_properties(df):
                         new_domain = domains[i].strip()
                         new_domain_obj = {"@id": "iudx:"+new_domain}
                         if new_domain_obj not in base_prop["@graph"][0]["iudx:domainIncludes"]:
+                            print("Other Schema property:", item[0], " updated with new domain ->", new_domain_obj )
                             base_prop["@graph"][0]["iudx:domainIncludes"].append(new_domain_obj)
-                            ordered_prop = order_obj(base_prop)
                             base_file.seek(0)
                             json.dump(ordered_prop, base_file, indent=4)
-                            print("Other Schema", base_property+".jsonld updated")
                             base_file.truncate()
+
             except:
-                print(f"{base_property} not found in Other Schema ")
+                print(f"NOT FOUND in Other Schema: {base_property}  ")
 
 
 if __name__=="__main__":
-    # file_dir = (os.path.abspath(os.getcwd()))
-    #
-    # print("dir_home", dir_home)
-    # print("file_dir ", file_dir)
-    domain_name = input("Enter the Domain name\n")
+
+    domain_name = input("Enter the Domain name (without trailing or inline spaces)\n")
     val = input("Do you want to add a new Domain (Y/N)\n").upper()
     if val =="Y":
         arr = next(os.walk(dir_home + "/data-models"))
         if domain_name in arr[1]:
             print("Domain already exists")
 
+
     sub_domain = Path(file_dir).stem
+
     model_name_dir = dir_home + "/data-models/" + domain_name 
+
     check_dir(model_name_dir)
     
     class_path = model_name_dir + "/classes" 
